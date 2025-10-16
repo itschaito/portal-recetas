fc5d80358277b1284a44599b9bea9dec9ac59d3c
itschaito <esladelpro@gmail.com>
2025-10-16 17:04:25 -0600

    /fix: correccion de errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import Navbar from './components/NavBar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import Recipes from './pages/Recipes'
